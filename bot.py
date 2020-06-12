8e94545350345f03f266456a2ce2be8ae173f684
manalda <manuel.alda@alumnos.uva.es>
2020-06-12 10:58:42 +0200

    Añadido el comando de reinicio al bot. Sin probar

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
 import telebot
 import env  
+import time
+import subprocess
 
-WELCOME_MESSAGE = "Hola, Sr."
+#Mensajes que el bot puede enviar al usuario
+WELCOME_MESSAGE = "Hola, Sr. Bienvenido de nuevo."
 STRANGE_PERSON_MESSAGE = "No me está permitido hablar con extraños :("
+REBOOT_MESSAGE = "Iniciando reinicio del dispositivo"
 NOT_IMPLEMENTED_MESSAGE = "Sin implementar"
+
+#Comandos de terminal que el bot puede llegar a ejecutar
+reboot = ["shutdown", "-r", "now"]
+
+bot = telebot.TeleBot(env.TOKEN)
 
 def known_id(id):
     return id == env.MY_CHAT_ID
-
-bot = telebot.TeleBot(env.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(m):
@@ -26,5 +33,13 @@ def help(m):
     else:
         bot.send_message(chat_id, STRANGE_PERSON_MESSAGE)
 
+@bot.message_handler(commands=['reboot'])
+def reboot_command(m):
+    chat_id = m.chat.id
+    if known_id(chat_id):
+        print("Iniciando reinicio del dispositivo...")
+        bot.send_message(chat_id, REBOOT_MESSAGE)
+        time.sleep(5)
+        subprocess.run(reboot)
 
 bot.polling()
